3e8e403823450eefde5842ef4cbaef3d56d70489
Dmytro Kagirov <dmytro27kagirov@gmail.com>
2022-08-22 19:56:30 +0200

    handlebars have been added.

---
+++ ch3_express/meadowlark_2_handlebars/meadowlark.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const { engine } = require('express-handlebars');
+
+const app = express()
+const port = process.env.PORT || 3000
+
+// Настройка механизма представлений Handlebars.
+app.engine('handlebars', engine({
+    defaultLayout: 'main',
+}))
+app.set('view engine', 'handlebars')
+app.set('views', './views');
+
+
+app.get('/', (req, res) => res.render('home'))
+
+app.get('/about', (req, res) => res.render('about'))
+
+// Пользовательская страница 404
+app.use((req, res) => {
+    res.status(404)
+    res.render('404')
+})
+
+// Пользовательская страница 500
+app.use((err, req, res, next) => {
+    console.error(err.message)
+    res.status(500)
+    res.render('500')
+})
+
+app.listen(port, () => console.log(
+    `Express web server started on http://localhost:${port}\n` +
+    `Press Ctrl+C for exit...`
+))
