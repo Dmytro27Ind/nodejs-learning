922eaec1b0db3a53c46fe28d4c3a1efa8e3e2e7a
Dmytro Kagirov <dmytro27kagirov@gmail.com>
2022-08-24 18:44:50 +0200

    express use with static files and handlebars

--- ch3_express/meadowlark_2_handlebars/meadowlark.js
+++ ch3_express/meadowlark_2_handlebars/meadowlark.js
@@ -4,6 +4,14 @@ const { engine } = require('express-handlebars');
 const app = express()
 const port = process.env.PORT || 3000
 
+const fortunes = [
+    "Победи свои страхи, или они победят тебя.",
+    "Рекам нужны истоки.",
+    "Не бойся неведомого.",
+    "Тебя ждет приятный сюрприз.",
+    "Будь проще везде, где только можно.",
+]
+
 // Настройка механизма представлений Handlebars.
 app.engine('handlebars', engine({
     defaultLayout: 'main',
@@ -11,10 +19,15 @@ app.engine('handlebars', engine({
 app.set('view engine', 'handlebars')
 app.set('views', './views');
 
+// for static files
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => res.render('home'))
 
-app.get('/about', (req, res) => res.render('about'))
+app.get('/about', (req, res) => {
+    const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
+    res.render('about', {fortune: randomFortune})
+})
 
 // Пользовательская страница 404
 app.use((req, res) => {
